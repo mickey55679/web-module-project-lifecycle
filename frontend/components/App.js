dc4c3c206b773d9504ad14ca65407affa92b468b
Michaiah Bos <michaiahbos@yahoo.com>
2023-11-14 17:15:31 -0600

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,66 +5,65 @@ const URL = 'http://localhost:9000/api/todos' // so we don't have to repeat this
 
 export default class App extends React.Component {
   constructor() {
-  super();
-  this.state = {
-    todos: [], 
-    error: '',
-    todoNameInput: '',
+    super();
+    this.state = {
+      todos: [],
+      error: "",
+      todoNameInput: "",
+    };
   }
-}
-onTodoNameInputChange = evt => {
-  const {value} = evt.target
-  this.setState({...this.state, todoNameInput: value })
-  
-}
-postNewTodo = () => {
-  axios.post(URL, {name: this.state.todoNameInput})
-  .then(res => {
-    this.fetchAllTodos()
-    this.setState({...this.state, todoNameInput: ''})
-  })
-  .catch(err => {
-    this.setState({...this.state, error: err.response.data.message })
-  })
-
-}
-onTodoFormSubmit = evt => {
-evt.preventDefault()
-this.postNewTodo()
-}
+  onTodoNameInputChange = (evt) => {
+    const { value } = evt.target;
+    this.setState({ ...this.state, todoNameInput: value });
+  };
+  resetForm = () => this.setState({ ...this.state, todoNameInput: "" });
+  setAxiosResponseError = err => this.setState({ ...this.state, error: err.response.data.message });
+  postNewTodo = () => {
+    axios
+      .post(URL, { name: this.state.todoNameInput })
+      .then((res) => {
+        this.fetchAllTodos();
+        this.resetForm();
+      })
+      .catch(this.setAxiosResponseError);
+  };
+  onTodoFormSubmit = (evt) => {
+    evt.preventDefault();
+    this.postNewTodo();
+  };
   fetchAllTodos = () => {
-  axios.get(URL) // this returns a promise
-  .then(res => {
-    this.setState({...this.state, todos: res.data.data})
-  })
-  .catch(err => {
-    this.setState({...this.state, error: err.response.data.message })
-    
-  })
-  } // arrow function bounds method to class 
+    axios
+      .get(URL) // this returns a promise
+      .then((res) => {
+        this.setState({ ...this.state, todos: res.data.data });
+      })
+      .catch(this.setAxiosResponseError);
+  }; // arrow function bounds method to class
   componentDidMount() {
-    this.fetchAllTodos()
+    this.fetchAllTodos();
     // fetch all todos from server
-  
-}
+  }
   render() {
     return (
       <div>
         <div id="error">Error: {this.state.error}</div>
         <div id="todos">
           <h2>Todos:</h2>
-          {
-            this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
-            })
-          }
+          {this.state.todos.map((td) => {
+            return <div key={td.id}>{td.name}</div>;
+          })}
         </div>
         <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
-          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder="Type todo"></input>
+          <input
+            value={this.state.todoNameInput}
+            onChange={this.onTodoNameInputChange}
+            type="text"
+            placeholder="Type todo"
+          ></input>
           <input type="submit"></input>
           <button>Clear completed</button>
         </form>
       </div>
-    )
+    );
   }
 }
