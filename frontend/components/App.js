7f29fd3155d67eabd56ebeb14d61e77d9befcd43
Michaiah Bos <michaiahbos@yahoo.com>
2023-11-14 14:57:31 -0600

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,6 +8,7 @@ export default class App extends React.Component {
   super();
   this.state = {
     todos: [], 
+    error: '',
   }
 }
   fetchAllTodos = () => {
@@ -16,6 +17,7 @@ export default class App extends React.Component {
     this.setState({...this.state, todos: res.data.data})
   })
   .catch(err => {
+    this.setState({...this.state, error: err.response.data.message })
     
   })
   } // arrow function bounds method to class 
@@ -27,7 +29,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div id="error">Error: No error here</div>
+        <div id="error">Error: {this.state.error}</div>
         <div id="todos">
           <h2>Todos:</h2>
           {
