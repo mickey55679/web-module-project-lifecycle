e54c8e24532f63301c3920ef02bd02174b96bb81
Michaiah Bos <michaiahbos@yahoo.com>
2023-11-15 11:31:02 -0600

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios' // in order to get this url we need this. 
 import Form from './Form'
+import TodoList from './TodoList'
 
 const URL = 'http://localhost:9000/api/todos' // so we don't have to repeat this all over the code
 
@@ -70,27 +71,11 @@ export default class App extends React.Component {
     return (
       <div>
         <div id="error">Error: {this.state.error}</div>
-        <div id="todos">
-          <h2>Todos:</h2>
-          {
-            this.state.todos.reduce((acc, td) => {
-              if (this.state.displayCompleteds || !td.completed)
-                return acc.concat(
-                  <div onClick={this.toggleCompleted(td.id)} key={td.id}>
-                    {td.name}
-                    {td.completed ? "✔️" : ""}
-                  </div>
-                );
-              return acc;
-            }, [])
-            // return (
-            //   <div onClick={this.toggleCompleted(td.id)} key={td.id}>
-            //     {td.name}
-            //     {td.completed ? " ✔️" : ""}
-            //   </div>
-            // );
-          }
-        </div>
+        <TodoList
+        todos={this.state.todos}
+        displayCompleteds={this.state.displayCompleteds}
+        toggleCompleted={this.toggleCompleted}
+         />
         <Form
           onTodoFormSubmit={this.onTodoFormSubmit}
           toggleDisplayCompleteds={this.toggleDisplayCompleteds}
